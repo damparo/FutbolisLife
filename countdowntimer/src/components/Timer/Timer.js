f20bf00b1a6c8e65c4eb1a384cc98b1be96333ca
Dennis Amparo <dennisamparo11@gmail.com>
2022-01-15 16:08:16 -0700

    had to update react version to latest

--- countdowntimer/src/components/Timer/Timer.js
+++ countdowntimer/src/components/Timer/Timer.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import './Timer.css';
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import "./Timer.css";
+import CardGroup from 'react-bootstrap/CardGroup';
+// import moment from "moment";
 
+function Timer() {
+  return (
+    <div>
+      {/* <Card>
 
-function Timer(){
-
-    return(
-
-        <div>
-
-            <p className='digitcolor'>time blocks will go here</p>
-
-        </div>
-    )
-
+      <p className="digitcolor">time blocks will go here</p>
+      </Card> */}
+      <CardGroup>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardGroup> 
+    </div>
+  );
 }
 
 export default Timer;
