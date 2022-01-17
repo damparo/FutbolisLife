b7065459854552ab1949914730505e94a7da2d3d
Dennis Amparo <dennisamparo11@gmail.com>
2022-01-17 14:39:24 -0700

    set up future date as prop

--- countdowntimer/src/components/Timer/Timer.js
+++ countdowntimer/src/components/Timer/Timer.js
@@ -1,38 +1,56 @@
 import React, { useState } from "react";
-import Card from 'react-bootstrap/Card';
+import { intervalToDuration, isBefore } from "date-fns";
+import Card from "react-bootstrap/Card";
 import "./Timer.css";
-import CardGroup from 'react-bootstrap/CardGroup';
+import CardGroup from "react-bootstrap/CardGroup";
 // import moment from "moment";
 
-function Timer() {
+function Timer({futureDate}) {
+
+    const now = new Date();
+    const isTimeUp = isBefore(futureDate, now);
+    let years = 0;
+    let months = 0;
+    let days = 0;
+
+    if(!isTimeUp){
+        const duration = intervalToDuration({
+            start: now,
+            end: futureDate
+        });
+
+        years = duration.years;
+        months = duration.months;
+        days = duration.days;
+
+    }
+
+    // const timerContents = isTimeUp ? ( <div className={ style.timeIsUp}>time is up!!!</div>) : (
+
+    // )
+
   return (
     <div>
       <CardGroup>
         <Card>
           <Card.Body>
-          <Card.Title>Years</Card.Title>
-            <Card.Text className="year">
-              0
-            </Card.Text>
+            <Card.Title>Years</Card.Title>
+            <Card.Text value={years} label="years" className="years"></Card.Text>
           </Card.Body>
         </Card>
         <Card>
           <Card.Body>
-          <Card.Title>Months</Card.Title>
-            <Card.Text className="months">
-              10
-            </Card.Text>
+            <Card.Title>Months</Card.Title>
+            <Card.Text value={months} label="days" className="months"></Card.Text>
           </Card.Body>
         </Card>
         <Card>
           <Card.Body>
-          <Card.Title>Days</Card.Title>
-            <Card.Text className="days">
-              15
-            </Card.Text>
+            <Card.Title>Days</Card.Title>
+            <Card.Text value={days} label="" className="days"></Card.Text>
           </Card.Body>
         </Card>
-      </CardGroup> 
+      </CardGroup>
     </div>
   );
 }
--- countdowntimer/src/components/WorldCupBoard/WorldCupBoard.js
+++ countdowntimer/src/components/WorldCupBoard/WorldCupBoard.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { add } from 'date-fns';
 import BannerText from '../BannerText/BannerText';
 import Timer from '../Timer/Timer';
 // import './WorldCupboard.css';
 
 
+const futureDate = add(new Date(), {
+
+    years: 0,
+    months: 9,
+    days: 35,
+
+});
+
 function WorldCupboard() {
     return (
         <div>
             <BannerText/>
-            <Timer/>
+            <Timer futureDate={futureDate}/>
         </div>
     )
 }
