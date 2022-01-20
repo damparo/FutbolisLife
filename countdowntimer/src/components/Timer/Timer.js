76c4d7f16f42e446fcde2a0b94043e3f94faed20
Dennis Amparo <dennisamparo11@gmail.com>
2022-01-19 20:48:12 -0700

    comments added

--- countdowntimer/src/components/Timer/Timer.js
+++ countdowntimer/src/components/Timer/Timer.js
@@ -25,13 +25,14 @@ function Timer() {
         // this variable gives the difference in time between the future world cup date and the present, in milliseconds
         const timeBetweenDates = startDate - now;
 
-        // convert the difference to years, days, hours, minutes, and seconds
+        // convert the difference in milliseconds to years, days, hours, minutes, and seconds
         const years = Math.floor(timeBetweenDates/(365*24*60*60*1000));
         const days = Math.floor(timeBetweenDates % (365*24*60*60*1000)/(24*60*60*1000));
         const hours = Math.floor(timeBetweenDates % (24*60*60*1000)/(60*60*1000));
         const minutes = Math.floor(timeBetweenDates % (60*60*1000)/(60*1000));
         const seconds = Math.floor(timeBetweenDates % (60*1000)/(1000));
 
+        // conditional to stop countdown after date is reached, otherwise keep updating via useState
         if (timeBetweenDates < 0){
           clearInterval(interval.current);
         } else{
@@ -42,16 +43,13 @@ function Timer() {
           setTimerMinutes(minutes);
           setTimerSeconds(seconds);
 
-
-
         }
 
       });
 
-      
-      
-    }
+    };
     
+    // useEffect hook to start countdown
     useEffect(() => {
 
       startCountDown();
